784071a0be3ed44e384ad0af6a15bfff0e02779f
azizisgandarzada <aziz.isgandarzada@ibar.az>
2021-10-18 17:36:45 +0400

    -- validations added

--- src/main/java/com/azizi/bank/dto/request/PaymentRequest.java
+++ src/main/java/com/azizi/bank/dto/request/PaymentRequest.java
@@ -1,7 +1,10 @@
 package com.azizi.bank.dto.request;
 
 import java.math.BigDecimal;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
-public record PaymentRequest(Integer userId, Integer merchantId, Integer cardId, BigDecimal amount) {
+public record PaymentRequest(@NotNull Integer userId, @NotNull Integer merchantId, @NotNull Integer cardId,
+                             @NotNull @Positive BigDecimal amount) {
 
 }
